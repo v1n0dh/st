4d9eeb281a2771f652df6830b8c6392c77fdfceb
v1n0dh <vinodhvinny27@gmail.com>
2025-07-03 15:16:57 +0000

    Changed Solarized dark theme

--- config.h
+++ config.h
@@ -124,34 +124,48 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* solarized dark */
-	"#073642",  /*  0: black    */
-	"#dc322f",  /*  1: red      */
-	"#859900",  /*  2: green    */
-	"#b58900",  /*  3: yellow   */
-	"#268bd2",  /*  4: blue     */
-	"#d33682",  /*  5: magenta  */
-	"#2aa198",  /*  6: cyan     */
-	"#eee8d5",  /*  7: white    */
-	"#002b36",  /*  8: brblack  */
-	"#cb4b16",  /*  9: brred    */
-	"#586e75",  /* 10: brgreen  */
-	"#657b83",  /* 11: bryellow */
-	"#839496",  /* 12: brblue   */
-	"#6c71c4",  /* 13: brmagenta*/
-	"#93a1a1",  /* 14: brcyan   */
-	"#fdf6e3",  /* 15: brwhite  */
-};
 
+  /* 8 normal colors */
+  [0] = "#002b36", /* black   */
+  [1] = "#dc322f", /* red     */
+  [2] = "#859900", /* green   */
+  [3] = "#b58900", /* yellow  */
+  [4] = "#268bd2", /* blue    */
+  [5] = "#6c71c4", /* magenta */
+  [6] = "#2aa198", /* cyan    */
+  [7] = "#93a1a1", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#657b83", /* black   */
+  [9]  = "#dc322f", /* red     */
+  [10] = "#859900", /* green   */
+  [11] = "#b58900", /* yellow  */
+  [12] = "#268bd2", /* blue    */
+  [13] = "#6c71c4", /* magenta */
+  [14] = "#2aa198", /* cyan    */
+  [15] = "#fdf6e3", /* white   */
+
+  /* special colors */
+  [256] = "#002b36", /* background */
+  [257] = "#93a1a1", /* foreground */
+};
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 12;
-unsigned int defaultbg = 8;
-unsigned int defaultcs = 14;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 15;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
